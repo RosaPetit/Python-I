c8b47cad7bf9af100f1483e0202e777c4f9da9df
Rosa Petit <83729618+RosaPetit@users.noreply.github.com>
2021-06-15 19:42:53 -0500

    Add files via upload
    Basic exercise of python

---
+++ BasicExercise 03.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2.6
+#_*_coding: utf8_*_ 
+
+from math import *
+
+def volumen(r): 
+   v = (4.0/3.0)*pi*(r**3)
+   print "El volumen es:"+ str(v)
+
+
+rad = eval (raw_input ("Introduzca el valor de R:"))
+
+  print "No sea bruto el radio debe ser positivo"
+
+else:
+  volumen(rad)    

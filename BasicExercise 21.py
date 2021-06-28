bed1af65551169c81fde047431ec2268ff644cda
Rosa Petit <83729618+RosaPetit@users.noreply.github.com>
2021-06-28 16:17:35 -0400

    Add files via upload

---
+++ BasicExercise 21.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2.6
+#_*_coding:utf8_*_
+
+#Ser importan las librerias  PIL
+
+from PIL import Image 
+
+#Tranformaciones geometricas (crea,rota,escala)
+
+img= Image.open("/home/rpetit/Imagenes/manut.png")
+
+
+#Rotacion 
+
+#img2=img.rotate(30)
+
+#img2=img.rotate(30, (expand=True))
+
+#Escal de imagen
+
+#img3=img.resize((400,500))
+
+#Interpolacion cuando de una imagen
+
+img4=img.resize((400,500),Image.ANTIALIAS)
+
+img4.show ()
+img.show()

f48b1ecc63b005136b7211ac8dfa7dac1b01ad29
Rosa Petit <83729618+RosaPetit@users.noreply.github.com>
2021-07-08 19:34:04 -0400

    Add files via upload

---
+++ 05 Elipse.py
@@ -0,0 +1,32 @@
+#_*_coding: utf-8_*_
+
+"Este es un programa basico que crea una ventana con las librerias Tkinter"
+
+#llamo al modulo Tkinter
+import Tkinter as Tki
+
+#Creo una ventana
+ventana = Tki.Tk()
+
+
+#Creo el lienzo y defino el ancho y el largo de la ventana en pixeles
+#Ancho
+W = 400
+#Altura
+H = 300
+lienzo = Tki.Canvas(ventana, width= W, height= H)
+lienzo.pack()
+
+
+#Dibujo una elipse
+x0 = 100
+y0 = 100
+x1 = 300
+y1 = 300
+lienzo.create_oval(x0,y0,x1,y1, fill = "blue")
+
+
+
+
+#Esto es necesario para dibujar la ventana
+ventana.mainloop()
